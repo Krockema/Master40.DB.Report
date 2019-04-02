6f7e7c53c12892d465bc29b48102257437b425ae
Martin Krockert <martin.krockert@gmail.com>
2019-04-02 09:56:40 +0200

    Add project files.

---
+++ Master40.DB.Report/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Master40.DB.Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
